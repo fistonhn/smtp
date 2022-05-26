079f7e9d438dcf47832b9c2b735aa28b1c161fcc
fistonhn <fistonhn@gmail.com>
2022-05-26 11:41:36 +0200

    new changes

--- musis/app.js
+++ musis/app.js
@@ -20,7 +20,11 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve('./build/', 'index.html'));
 });
 
-app.listen(5000, () => {
-    console.log(`Server is listening on port ${5000}`);
-  });
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+// app.listen(5000, () => {
+//     console.log(`Server is listening on port ${5000}`);
+//   });
   
